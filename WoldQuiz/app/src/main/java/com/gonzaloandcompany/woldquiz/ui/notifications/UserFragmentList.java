38f3c7b05b9820a649e698ce098c203f62b1964c
Lucas Amado <amadolucas99@gmail.com>
2020-02-04 12:38:29 +0100

    order de mas a menos puntos

--- WoldQuiz/app/src/main/java/com/gonzaloandcompany/woldquiz/ui/notifications/UserFragmentList.java
+++ WoldQuiz/app/src/main/java/com/gonzaloandcompany/woldquiz/ui/notifications/UserFragmentList.java
@@ -76,7 +76,7 @@ public class UserFragmentList extends Fragment {
             Collections.sort(userList, new Comparator<User>() {
                 @Override
                 public int compare(User u1, User u2) {
-                    return (u1.getPuntos() - u2.getPuntos());
+                    return -(u1.getPuntos() - u2.getPuntos());
                 }
             });
 
