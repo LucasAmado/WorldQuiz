b5e278ce85ce1ab4eab89fdecd42c3c362ecf325
joseluis <joseluis10cortes@gmail.com>
2020-02-05 14:26:49 +0100

    fragment maps

--- WoldQuiz/app/src/main/java/com/gonzaloandcompany/woldquiz/LoginActivity.java
+++ WoldQuiz/app/src/main/java/com/gonzaloandcompany/woldquiz/LoginActivity.java
@@ -4,12 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -23,7 +20,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,7 +30,6 @@ public class LoginActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     SignInButton btn_login;
     FirebaseUser user;
-    Button btn_logout;
     ProgressBar progressBar;
     GoogleSignInClient mGoogleSignInClient;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
