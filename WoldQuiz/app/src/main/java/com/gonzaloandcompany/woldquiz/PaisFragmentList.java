d5cb2e9cf6265f140d655cd1b171ed8df44a5017
Lucas Amado <amadolucas99@gmail.com>
2020-02-06 19:41:33 +0100

    filtro de países por moneda empezado

--- WoldQuiz/app/src/main/java/com/gonzaloandcompany/woldquiz/PaisFragmentList.java
+++ WoldQuiz/app/src/main/java/com/gonzaloandcompany/woldquiz/PaisFragmentList.java
@@ -1,21 +1,37 @@
 package com.gonzaloandcompany.woldquiz;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gonzaloandcompany.woldquiz.dummy.DummyContent;
 import com.gonzaloandcompany.woldquiz.dummy.DummyContent.DummyItem;
+import com.gonzaloandcompany.woldquiz.models.Pais;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 /**
  * A fragment representing a list of Items.
@@ -25,11 +41,17 @@ import java.util.List;
  */
 public class PaisFragmentList extends Fragment {
 
-    // TODO: Customize parameter argument names
+
     private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
+
     private int mColumnCount = 1;
     private OnListFragmentInteractionListener mListener;
+    private List<String> listaMonedas;
+    //TODO borrar
+    private PaisService service;
+    private List<Pais> listaPaises = new ArrayList<>();
+    RecyclerView recyclerView;
+    MyPaisRecyclerViewAdapter adapter;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -37,6 +59,41 @@ public class PaisFragmentList extends Fragment {
      */
     public PaisFragmentList() {
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_pais_list, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.filterIcon:
+                //TODO hablar con miguel
+                /*DialogFragment dialog = new FilterPaisDialogFragment();
+                dialog.show(getSupportFragmentManager(), "FilterPaisDialogFragment");*/
+                new LoadPaisesByFiltro().execute();
+                break;
+
+            case R.id.searchIcon:
+                break;
+
+            case R.id.quizIcon:
+                Intent i = new Intent(getActivity(), QuizActivity.class);
+                startActivity(i);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     // TODO: Customize parameter initialization
     @SuppressWarnings("unused")
@@ -107,5 +164,41 @@ public class PaisFragmentList extends Fragment {
     public interface OnListFragmentInteractionListener {
         // TODO: Update argument type and name
         void onListFragmentInteraction(DummyItem item);
+    }
+
+    private class LoadPaisesByFiltro extends AsyncTask<String, Void, List<Pais>> {
+
+        @Override
+        protected List<Pais> doInBackground(String... strings) {
+
+            Call<List<Pais>> callPaises = null;
+
+            for (String moneda : listaMonedas) {
+                if (listaMonedas.contains(moneda)) {
+                    callPaises = service.listPaisesByMoneda(strings[0]);
+                }
+            }
+
+            Response<List<Pais>> responsePaises = null;
+
+            try {
+                responsePaises = callPaises.execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (responsePaises.isSuccessful()) {
+                listaPaises = responsePaises.body();
+            }
+            return listaPaises;
+        }
+
+        @Override
+        protected void onPostExecute(List<Pais> paises) {
+            if (paises != null) {
+                //adapter = new MyPaisRecyclerViewAdapter();
+                recyclerView.setAdapter(adapter);
+            }
+        }
     }
 }
