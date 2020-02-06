f9fcaeb5b065bb99d6cf4f4c463ac60561ab7f06
Lucas Amado <amadolucas99@gmail.com>
2020-02-06 17:29:53 +0100

    retrofit paises. Añadido el filtrar por moneda e idioma

--- WoldQuiz/app/src/main/java/com/gonzaloandcompany/woldquiz/ServiceGeneratorPais.java
+++ WoldQuiz/app/src/main/java/com/gonzaloandcompany/woldquiz/ServiceGeneratorPais.java
@@ -10,7 +10,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGeneratorPais {
-    private static final String BASE_URL = "https://restcountries.eu/";
+    private static final String BASE_URL = "https://restcountries.eu/rest/v2/";
 
     private static Retrofit retrofit = null;
 
