c4327da3463b0ce0f51268a0d8c880959ed4f1ca
Esperanza Escacena <espemacem@gmail.com>
2020-02-08 21:20:56 +0100

    hemos apañado un filtro de moneda

---
+++ WoldQuiz/app/src/main/java/com/gonzaloandcompany/woldquiz/ui/home/DialogPassData.java
@@ -0,0 +1,7 @@
+package com.gonzaloandcompany.woldquiz.ui.home;
+
+import com.gonzaloandcompany.woldquiz.models.Pais;
+
+public interface DialogPassData {
+    void filterByCoin(String coinName);
+}
